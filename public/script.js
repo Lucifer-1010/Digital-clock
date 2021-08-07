ddc1b72addc8b6fa5e28d1e5149a8649146c1d19
Atharv Puranik <68159253+Lucifer-1010@users.noreply.github.com>
2021-08-07 12:35:48 +0530

    Added Code

---
+++ public/script.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+	setInterval(updateTime, 1000)
+};
+
+function updateTime() {
+	const format = number => `${ +!(number < 10) && '' }${number}`;
+	
+	let date = new Date();
+	
+	document.querySelector("#hour")  .innerHTML = format( date.getHours()   );
+	document.querySelector("#minute").innerHTML = format( date.getMinutes() );
+	document.querySelector("#second").innerHTML = format( date.getSeconds() );
+
+	document.querySelector("#date").innerHTML = (
+		[
+            date.getDate()+'-'+(date.getMonth()+1)+'-'+date.getFullYear()
+			// date.getDate(), 
+			// ++date.getMonth(), 
+			// date.getFullYear()
+		]
+	);
+	
+	const week = [
+		"Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"
+	];
+	
+	document.querySelector("#day").innerHTML = week[ date.getDay() ];
+}
+
+
+
+
+
+
+
